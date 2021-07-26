08d3f96230da0189290176f460551d37019f5549
Antonio <antonioru1997@gmail.com>
2021-07-26 15:57:39 +0200

    Numeri utente +verifiche (5 tentativi per testare)

--- js/main.js
+++ js/main.js
@@ -9,20 +9,47 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 
 //Dichiaro variabili
-var cpuNumber = [];
+var cpuNumbers = [];
+var userNumbers = [];
+var userNumber;
+var attempts = 5; //per testare ho messo 5 tentativi, alla fine "alzerò" ad 84.
+var foundNumber = false;
+var score = 0;
+
 
 //La CPU genera i 16 numeri casuali compresi da 1 a 100
 
 // Creo ciclo while dove indico anche la condizione per inserire un numero solo se non è già presente nell'array.
-while (cpuNumber.length < 16) {
+while (cpuNumbers.length < 16) {
     var randomNumber = randomNumberGenerator(1, 100);
-    var findNumber = findInArray(cpuNumber, randomNumber);
+    var findNumber = findInArray(cpuNumbers, randomNumber);
     if (findNumber == false) {
-        cpuNumber.push(randomNumber);
+        cpuNumbers.push(randomNumber);
     }
 }
+console.log("Numeri Bomba: " + cpuNumbers);
 
-console.log(cpuNumber)
+// Chiedo all'utente di inserire uno alla volta un numero con 84 tentativi a disposizione.
+
+while (userNumbers.length < attempts && foundNumber == false) {
+     userNumber = parseInt(prompt("Inserisci un numero da 1 a 100"));
+    wrongNumber();
+  
+    if (findInArray(userNumbers, userNumber) == false) {
+        // Se il numero non è presente viene normalmente inviato.
+        userNumbers.push(userNumber);
+        // Se il numero è nella lista della CPU la partite si conclude.
+        if (findInArray(cpuNumbers, userNumber) == true) {
+        console.log("partita finita");
+        foundNumber = true;
+        } else {
+        score+= 1;
+        }
+    }
+  }
+console.log("score: " + score)
+
+/* ----- FUNZIONI ----- */
 
 // Creo funzione per generare un numero casuale.
 function randomNumberGenerator(min, max) {
@@ -38,6 +65,23 @@ function findInArray(array, element) {
         result = true;
       }
       i++;
+    }
+    return result;
+}
+
+// Funzione per RIchiedere un numero corretto in caso di numero errato.
+function wrongNumber () {
+    while (correctNumberCheck(1, 100, userNumber) == false) {
+        userNumber = parseInt(prompt("!!! NUMERO ERRATO. INSERIRE UN NUMERO DA 1 A 100 !!!"));
+        console.log(userNumber);
+    }
+}
+
+// Funzione per verificare che il numero sia compreso veramente da 1 a 100. (altrimenti viene chiesto di nuovo)
+function correctNumberCheck(min, max, number) {
+    var result = false;
+    if (number >= min && number <= max) {
+        result = true;
     }
     return result;
 }
